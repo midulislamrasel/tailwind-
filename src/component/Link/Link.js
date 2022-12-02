90f06f2e7562e84a01d5b50df3ef14a58ffdf841
Midul-islam-Russell <midulraddy121@gmail.com>
2022-12-02 20:17:09 +0600

    Remove

--- src/component/Link/Link.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-
-const Link = ({route}) => {
-    return (
-            <li ><a className='m-4 text-5xl' href={route.path}>{route.Name}</a></li> 
-    );
-};
-
-export default Link;
